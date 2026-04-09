a235a2a9b74440c410f221a6f4ee73d750bfb1d1
krithisekar <58632168+krithisekar@users.noreply.github.com>
2026-04-09 14:34:11 +0530

    Change greeting from 'Dinesh' to 'Krithika'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Dinesh! Node.js app is running 🚀');
+    res.end('Hello Krithika! Node.js app is running 🚀');
 });
 
 server.listen(PORT, () => {
